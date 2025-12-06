94f81390d8569f8a2e1194eb28d5b91c73f2fd60
Roman <neirity@gmail.com>
2025-12-06 11:59:23 +0200

    fix layout props

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  notepreviev,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
-  notepreviev: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -35,7 +35,7 @@ export default function RootLayout({
           <Header />
           <main>
             {children}
-            {notepreviev}
+            {modal}
           </main>
           <Footer />
         </TanStackProvider>
